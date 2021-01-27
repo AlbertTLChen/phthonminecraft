6a95dfc30949dc6d226cc8567fe5b1f617758f7f
AlbertTLChen <77959978+AlbertTLChen@users.noreply.github.com>
2021-01-27 16:45:35 +0800

    Add files via upload

---
+++ 3-5.py
@@ -0,0 +1,6 @@
+from mcpi.minecraft import Minecraft
+mc= Minecraft.create()
+
+x,y,z=mc.player.getTilePos()
+for i in range(0,100):
+    mc.setBlock(x,y-1,z+i,41)
---
+++ practice2.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc= Minecraft.create()
+
+while True:
+    hits=mc.events.pollProjectileHits()
+    if len(hits)>0:
+        a=hits[0]
+        x,y,z=a.pos.x,a.pos.y,a.pos.z
+        mc.createExplosion(x,y,z,power=100)
+        mc.player.setTilePos(x,y,z)
+        mc.spawnEntity(x,y,z,99)
